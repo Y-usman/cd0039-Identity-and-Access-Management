16e294819735717e55ed215eaf06969b152fa370
Yusuf <uthmanyusuf1998@gmail.com>
2022-07-17 18:23:53 +0100

    more updates

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-55w3eouq.eu.auth0.com', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
+    url: 'dev-55w3eouq.eu', // the auth0 domain prefix
+    audience: 'coffee-shop-api', // the audience set for the auth0 app
     clientId: 'eyQl0dtm4p7YCogSjX60Gn080EVojF8A', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
