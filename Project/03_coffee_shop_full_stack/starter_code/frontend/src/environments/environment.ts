10faf6d5dc7c467f511194b6b87c1fa8e1269003
Yusuf <uthmanyusuf1998@gmail.com>
2022-07-17 18:02:40 +0100

    enviroment variables added

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
+    url: 'dev-55w3eouq.eu.auth0.com', // the auth0 domain prefix
     audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    clientId: 'eyQl0dtm4p7YCogSjX60Gn080EVojF8A', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
