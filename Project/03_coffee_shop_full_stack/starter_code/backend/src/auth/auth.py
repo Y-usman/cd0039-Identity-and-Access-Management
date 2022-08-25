685d01d134b7b0bbd84468c03c9fe75e20c14f7e
Yusuf <uthmanyusuf1998@gmail.com>
2022-08-25 11:38:47 +0100

    Auth Domain Update

--- Project/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ Project/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -5,9 +5,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'dev-tekxhwwg.us.auth0.com'
+AUTH0_DOMAIN = 'https://dev-tekxhwwg.us.auth0.com/'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'dev'
+API_AUDIENCE = 'coffee-shop-api'
 
 ## AuthError Exception
 '''
